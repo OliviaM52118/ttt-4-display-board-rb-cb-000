b96ed16c4e31b70e5d7dd2267dea0f43bd5d54fb
Olivia Macedo <liom62313@gmail.com>
2018-08-18 23:22:51 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,6 +5,6 @@ def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
   puts " "+board[0]+" " + "|" + " "+board[1]+" " + "|" + " "+board[2]+" "
   puts "-----------"
   puts " "+board[0]+" " + "|" + " "+board[1]+" " + "|" + " "+board[2]+" "
-    puts "-----------"
+  puts "-----------"
   puts " "+board[0]+" " + "|" + " "+board[1]+" " + "|" + " "+board[2]+" "
 end

2d62168b8a3e6f7d303372626006c034e3abbb6a
Olivia Macedo <liom62313@gmail.com>
2018-08-18 23:21:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,5 +2,7 @@
 # out the current state.
 board = ["X", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board = [" ", " ", " ", " ", " ", " ", " ", " ", " "])
-
+  puts
+  puts
+  puts
 end
